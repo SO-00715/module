c13c3d1a9ed9d39679f610bd344e9c456560aa01
SO-00715 <mikan.chocolate.inu.1asj@gmail.com>
2020-10-26 23:42:59 +0900

    modal.js add clickEvent
    .js-show-modal.coverのclickイベント追加。

--- modal/modal.js
+++ modal/modal.js
@@ -1,3 +1,4 @@
+// jsで書きたい
 $(function() {
     $('.js-show-modal').on('click', function() {
         let modalWidth = $('.js-show-modal-target').width();
@@ -7,7 +8,12 @@ $(function() {
         $('.js-show-modal-cover').show();
     });
 
+    // 閉じるボタン、モーダル背景どちらを押してもモーダルが閉じるようにしたい。
     $('.js-hide-modal').on('click', function() {
+        $('.js-show-modal-target').hide();
+        $('.js-show-modal-cover').hide();
+    });
+    $('.js-show-modal-cover').on('click', function(){
         $('.js-show-modal-target').hide();
         $('.js-show-modal-cover').hide();
     });
