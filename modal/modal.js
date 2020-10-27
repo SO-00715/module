ada2a44377ce9c340c1d5803ea14bba7f1fdd9df
SO-00715 <mikan.chocolate.inu.1asj@gmail.com>
2020-10-27 18:45:55 +0900

    modal dir update
    モーダル画面をcssで記述。modal.js 不要なコード削除。モーダル背景クリックでも閉じる仕様に変更。

--- modal/modal.js
+++ modal/modal.js
@@ -1,20 +1,36 @@
 // jsで書きたい
 $(function() {
     $('.js-show-modal').on('click', function() {
-        let modalWidth = $('.js-show-modal-target').width();
-        let windowWidth = $(window).width();
-        $('.js-show-modal-target').attr('style', 'margin-left: ' + (windowWidth/2 - modalWidth/2) + 'px');
+        // let modalWidth = $('.js-show-modal-target').width();
+        // let windowWidth = $(window).width();
+        // $('.js-show-modal-target').attr('style', 'margin-left: ' + (windowWidth/2 - modalWidth/2) + 'px');
         $('.js-show-modal-target').show();
         $('.js-show-modal-cover').show();
     });
 
-    // 閉じるボタン、モーダル背景どちらを押してもモーダルが閉じるようにしたい。
+    // 閉じるボタン、モーダル背景どちらを押してもモーダルが閉じるようにしたい。クラス名つけたらOK
     $('.js-hide-modal').on('click', function() {
         $('.js-show-modal-target').hide();
         $('.js-show-modal-cover').hide();
     });
-    $('.js-show-modal-cover').on('click', function(){
-        $('.js-show-modal-target').hide();
-        $('.js-show-modal-cover').hide();
-    });
 });
+
+// window.addEventListener('DOMContentLoaded', function() {
+//     new Modal();
+// });
+
+// class Modal {
+//     constructor() {
+//         this.btn = document.querySelector('.js-show-modal');
+//         this.modalCover = document.querySelector('.js-show-modal-cover');
+//         this._showModal();
+//         // this._hideModal();
+//     }
+//     _showModal() {
+//         this.btn.addEventListener('click', function() {
+//             let modal = document.querySelector('.modal');
+//             modal.display = 'block';
+//             console.log(modal);
+//         })
+//     }
+// }
